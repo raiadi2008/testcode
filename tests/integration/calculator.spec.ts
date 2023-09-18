b090ac9266843598208e9c54a1997634ae3eef9e
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-09-18 23:46:00 +0530

    multiple instance test case added

--- tests/integration/calculator.spec.ts
+++ tests/integration/calculator.spec.ts
@@ -9,7 +9,6 @@ import {
   OperationRequest,
 } from "../../types/requests"
 import HttpStatus from "../../constants/status_code"
-import { Server, IncomingMessage, ServerResponse } from "node:http"
 
 describe("initialize calculator", () => {
   it("initialize calculator with Multiplication", async () => {
@@ -62,49 +61,49 @@ describe("initialize calculator", () => {
       .to.have.property("calculator_instance_id")
       .that.is.a("string")
   })
-})
 
-it("initialize calculator with Divison", async () => {
-  const resp = await request(app)
-    .post("/api/v1/init")
-    .send({
+  it("initialize calculator with Divison", async () => {
+    const resp = await request(app)
+      .post("/api/v1/init")
+      .send({
+        operand_first: 4,
+        operand_second: 5,
+        operator: AllowedOperations.DIVISION,
+      } as InitializeCalculatorRequest)
+
+    expect(resp.status).to.equal(HttpStatus.CREATED)
+    expect(resp.body.result).to.equal(0.8)
+    expect(resp.body.total_ops).to.equal(1)
+    expect(resp.body)
+      .to.have.property("calculator_instance_id")
+      .that.is.a("string")
+  })
+
+  it("initialize calculator with Invalid operation", async () => {
+    const resp = await request(app).post("/api/v1/init").send({
       operand_first: 4,
       operand_second: 5,
-      operator: AllowedOperations.DIVISION,
-    } as InitializeCalculatorRequest)
+      operator: "adfasd",
+    })
 
-  expect(resp.status).to.equal(HttpStatus.CREATED)
-  expect(resp.body.result).to.equal(0.8)
-  expect(resp.body.total_ops).to.equal(1)
-  expect(resp.body)
-    .to.have.property("calculator_instance_id")
-    .that.is.a("string")
-})
-
-it("initialize calculator with Invalid operation", async () => {
-  const resp = await request(app).post("/api/v1/init").send({
-    operand_first: 4,
-    operand_second: 5,
-    operator: "adfasd",
+    expect(resp.status).to.equal(HttpStatus.BAD_REQUEST)
   })
 
-  expect(resp.status).to.equal(HttpStatus.BAD_REQUEST)
-})
+  it("initialize calculator with empty body operation", async () => {
+    const resp = await request(app).post("/api/v1/init").send()
 
-it("initialize calculator with empty body operation", async () => {
-  const resp = await request(app).post("/api/v1/init").send()
-
-  expect(resp.status).to.equal(HttpStatus.BAD_REQUEST)
-})
-
-it("initialize calculator with wrong operand", async () => {
-  const resp = await request(app).post("/api/v1/init").send({
-    operand_first: "adfadsf",
-    operand_second: "asdfasd",
-    operator: "adfasd",
+    expect(resp.status).to.equal(HttpStatus.BAD_REQUEST)
   })
 
-  expect(resp.status).to.equal(HttpStatus.BAD_REQUEST)
+  it("initialize calculator with wrong operand", async () => {
+    const resp = await request(app).post("/api/v1/init").send({
+      operand_first: "adfadsf",
+      operand_second: "asdfasd",
+      operator: "adfasd",
+    })
+
+    expect(resp.status).to.equal(HttpStatus.BAD_REQUEST)
+  })
 })
 
 describe("perform operations", () => {
@@ -291,5 +290,121 @@ describe("Reset and Undo", () => {
     expect(resp.body.message).to.equal(
       `calculator ${calculator_instance} has been reset`
     )
+  })
+})
+
+describe("Mutliple calculator instance", () => {
+  let calculator_instance_1: string
+  let calculator_instance_2: string
+  let calculator_instance_3: string
+  let calculator_instance_4: string
+
+  it("initialize calculator with Multiplication", async () => {
+    const resp = await request(app)
+      .post("/api/v1/init")
+      .send({
+        operand_first: 4,
+        operand_second: 5,
+        operator: AllowedOperations.MULTIPLY,
+      } as InitializeCalculatorRequest)
+
+    expect(resp.status).to.equal(HttpStatus.CREATED)
+    expect(resp.body.result).to.equal(20)
+    expect(resp.body.total_ops).to.equal(1)
+    expect(resp.body)
+      .to.have.property("calculator_instance_id")
+      .that.is.a("string")
+
+    calculator_instance_1 = resp.body.calculator_instance_id
+  })
+
+  it("initialize calculator with Addition", async () => {
+    const resp = await request(app)
+      .post("/api/v1/init")
+      .send({
+        operand_first: 4,
+        operand_second: 5,
+        operator: AllowedOperations.ADD,
+      } as InitializeCalculatorRequest)
+
+    expect(resp.status).to.equal(HttpStatus.CREATED)
+    expect(resp.body.result).to.equal(9)
+    expect(resp.body.total_ops).to.equal(1)
+    expect(resp.body)
+      .to.have.property("calculator_instance_id")
+      .that.is.a("string")
+
+    calculator_instance_2 = resp.body.calculator_instance_id
+  })
+
+  it("initialize calculator with Subtraction", async () => {
+    const resp = await request(app)
+      .post("/api/v1/init")
+      .send({
+        operand_first: 4,
+        operand_second: 5,
+        operator: AllowedOperations.SUBTRACT,
+      } as InitializeCalculatorRequest)
+
+    expect(resp.status).to.equal(HttpStatus.CREATED)
+    expect(resp.body.result).to.equal(-1)
+    expect(resp.body.total_ops).to.equal(1)
+    expect(resp.body)
+      .to.have.property("calculator_instance_id")
+      .that.is.a("string")
+
+    calculator_instance_3 = resp.body.calculator_instance_id
+  })
+
+  it("initialize calculator with Divison", async () => {
+    const resp = await request(app)
+      .post("/api/v1/init")
+      .send({
+        operand_first: 4,
+        operand_second: 5,
+        operator: AllowedOperations.DIVISION,
+      } as InitializeCalculatorRequest)
+
+    expect(resp.status).to.equal(HttpStatus.CREATED)
+    expect(resp.body.result).to.equal(0.8)
+    expect(resp.body.total_ops).to.equal(1)
+    expect(resp.body)
+      .to.have.property("calculator_instance_id")
+      .that.is.a("string")
+
+    calculator_instance_4 = resp.body.calculator_instance_id
+  })
+
+  it("reset first instance", async () => {
+    const resp = await request(app).get(
+      `/api/v1/reset/${calculator_instance_1}`
+    )
+    expect(resp.status).to.equal(HttpStatus.SUCCESS)
+    expect(resp.body.success).to.equal(true)
+    expect(resp.body.message).to.equal(
+      `calculator ${calculator_instance_1} has been reset`
+    )
+  })
+
+  it("add number to second instance", async () => {
+    const resp = await request(app)
+      .post("/api/v1/operation")
+      .send({
+        operand: 10,
+        operator: AllowedOperations.ADD,
+        calculator_instance_id: calculator_instance_2,
+      } as OperationRequest)
+
+    expect(resp.status).to.equal(HttpStatus.SUCCESS)
+    expect(resp.body.result).to.equal(19)
+    expect(resp.body.total_ops).to.equal(2)
+  })
+
+  it("undo third instance", async () => {
+    const resp = await request(app).put(`/api/v1/undo/${calculator_instance_3}`)
+    console.log(resp.body)
+    expect(resp.status).to.equal(HttpStatus.SUCCESS)
+    expect(resp.body.result).to.equal(0)
+    expect(resp.body.total_ops).to.equal(0)
   })
 })
